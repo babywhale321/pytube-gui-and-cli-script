d26df23bc222747f815140dd46b8fb03d360ac87
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2023-01-31 20:11:34 -0500

    Update alternative.py

--- alternative.py
+++ alternative.py
@@ -1,31 +1,17 @@
-from pytube import YouTube
-import requests
-import re
-import subprocess
-import shlex
+while True:
+    chanloop = input ("Would you like to download an entire channel or just a video?  c/v")
+    if chanloop == "c":
+        chanlink = input("Enter the Channel link:   ")
 
-channel_request = requests.get("https://www.youtube.com/@MentalOutlaw/videos")
+        channel_request = requests.get(chanlink)
+        pull_data = channel_request.text
+        regex = r"\"https://www.youtube.com/channel/.*?\",\""
+        matches = re.search(regex, pull_data)
+        url_end = (matches.group())
+        url_end = str(url_end)
+        chanurl = (url_end.lstrip("\"").rstrip("\",\""))
 
-pull_data = channel_request.text
-
-regex = r"\"videoId\":\"...........\""
-
-matches = re.findall(regex, pull_data)
-
-matches = str(matches)
-
-regex2 = r"[0-9a-zA-Z]{11}"
-
-matches2 = re.search(regex2, matches)
-
-url_end = (matches2.group())
-
-url_front = ("https://www.youtube.com/watch?v="+url_end)
-
-print(url_front)
-
-user_ask = input ("Wana download it?")
-if user_ask == "y":
-    subprocess.run(["pytube",url_front])
-else:
-    print("k bye")
+        ytchan = Channel(chanurl)
+        print (f"Downloading videos from: {ytchan.channel_name} ")
+    else:
+        break
