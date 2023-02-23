e3d97bf2ede903a3c77cb0b2035cb9e5c8205503
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2023-02-22 22:00:10 -0500

    Update gui-experimental.py

--- testing-and-experimental/gui-experimental.py
+++ testing-and-experimental/gui-experimental.py
@@ -2,42 +2,23 @@
 import requests
 import re
 from pytube import YouTube, Channel
-from pytube.cli import on_progress
 import os
 from easygui import *
-
-#colors for text
-class bcolors:
-    OKGREEN = '\033[92m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
 
 # Establishing functions to be used in code
 # This function will download a single video after user provides a link.
 def download_video(link, resochoice):
-    # Using a try/except to handle potential errors.
-    try:
-        # Creating a variable to store the YouTube object for ease of use later.
-        yt = YouTube(link, on_progress_callback=on_progress)
-        # Showing details of the selected video.
-        print(f"Title: {yt.title}")
-        print(f"Number of views: {yt.views}")
-        print(f"Length of video: {yt.length}")
-        print(f"Rating of video: {yt.rating}")
 
-        print("Downloading...\n")
+    # Creating a variable to store the YouTube object for ease of use later.
+    yt = YouTube(link)
+    # Showing details of the selected video.
+    
+    if resochoice in ("l","low"):
+        yt.streams.first().download()
         
-        if resochoice in ("l","low"):
-            yt.streams.first().download()
-            print("\n")
+    elif resochoice in ("h","high"):
+        yt.streams.get_highest_resolution().download()
             
-        elif resochoice in ("h","high"):
-            yt.streams.get_highest_resolution().download()
-            print("\n")
-            
-    except Exception as e:
-        # Notifies user of any error (i.e. video is private or unlisted etc.)
-        print(f"An error occurred: {e}")
 
 # This function will download all videos from a channel or only the new videos that have not previously been downloaded.
 def download_channel(chanlink, resochoice):
@@ -55,21 +36,15 @@ def download_channel(chanlink, resochoice):
     try:
 
         # If not a new channel this will compare a log .txt file to a list of video URLs currently hosted on the channel.
-        print("Trying to open" ,ytchan.channel_name + "new.txt\n")
         with open(ytchan.channel_name + "new.txt", "w") as file:
             for url in ytchan.video_urls:
                 file.write(str(url) + "\n")        
-        print(bcolors.OKGREEN + "Opening" ,ytchan.channel_name + "new.txt","was successful\n" + bcolors.ENDC)
-           
-        print("Trying to open" ,ytchan.channel_name + "old.txt\n")        
+                   
         with open(ytchan.channel_name + 'old.txt', 'r') as oldlist:
-            videolist1 = oldlist.read()
-        print(bcolors.OKGREEN + "Opening" ,ytchan.channel_name + "old.txt","was successful\n" + bcolors.ENDC)  
+            videolist1 = oldlist.read()  
         
-        print("Creating new updated list under", ytchan.channel_name + "new.txt\n")
         with open(ytchan.channel_name + 'new.txt', 'r') as newlist:
             videolist2 = newlist.read()
-        print(bcolors.OKGREEN + "Creating" ,ytchan.channel_name + "new.txt","was successful\n" + bcolors.ENDC) 
             
         # Saves all the new URLs into a list
         downloadlist = []
@@ -79,9 +54,8 @@ def download_channel(chanlink, resochoice):
                 downloadlist.append(url)
                 
         # Downloads all the new videos in the selected resolution.
-        print("Downloading... this may take some time...\n")
         for url in downloadlist:
-            yt = YouTube(url, on_progress_callback=on_progress)
+            yt = YouTube(url)
             if resochoice in ("l","low"):
                 yt.streams.first().download()
             elif resochoice in ("h","high"):
@@ -94,93 +68,153 @@ def download_channel(chanlink, resochoice):
         os.rename(oldname, newname)
 
     except:
-            print(bcolors.FAIL + "Opening" ,ytchan.channel_name + "old.txt","has failed\n" +  bcolors.ENDC)   
-            print("Assumeing new channel list of videos\n")
             # If it is a new channel, creates a file called <channelname>old.txt and writes all video urls on channel to it as a log.
         
-            print("Creating" ,ytchan.channel_name + "old.txt\n")   
             with open(ytchan.channel_name + "old.txt", "w") as file:
                 for url in ytchan.video_urls:
                     file.write(str(url) + "\n")
-            print(bcolors.OKGREEN + "Creating" ,ytchan.channel_name + "old.txt","was successful\n" +  bcolors.ENDC)  
             
-            print("Downloading... this may take some time...\n")
             # Downloads all videos on channel in selected resolution.
             for video in ytchan.videos:
                 if resochoice in ("l","low"):
                     video.streams.first().download()
                 elif resochoice in ("h","high"):
                     video.streams.get_highest_resolution().download()                         
-    print("Finished Downloading.")
+
 
 # This is the actual program.
-choices = ["Channel", "Video", "Exit"]
-msg = "Select Channel to download an entire channel\nSelect video to download a single video."
-title = "python pytube gui"
-# opening a choice box using our msg,title and choices
-chanloop = choicebox(msg, title, choices = choices)
 
-#if video is selected
-if chanloop == ("Video"):
+while True:
     
-    #Getting video link.
-    text = "What is the YouTube url?"
-    title = "url selection"
-    link = enterbox(text,title)
+    choices = ["Channel", "Video", "Exit"]
+    msg = "Select Channel to download an entire channel\nSelect video to download a single video\n\nNOTE: the downloaded video or channel will be saved in the home directory"
+    title = "Pytube GUI"
     
-    #exit on cancel
-    if link == None:
-        exit()
-    
+    # opening a choice box using our msg,title and choices
+    chanloop = choicebox(msg, title, choices = choices)
+
+    #if video is selected
+    if chanloop == ("Video"):
+        
+        #Getting video link.
+        text = "What is the YouTube URL?"
+        title = "URL Selection"
+        link = enterbox(text,title)
+        
+        #exit on cancel
+        if link == None:
+            break
+        
+        #Getting resolution choice.
+        choices = ["Low","High"]
+        title = "Youtube Download Quality"
+        msg = "What will be the download quality?"
+        reply = choicebox(msg,title, choices = choices)
+        
+        #low will equal l and high will equal h
+        if reply == "Low":
+            resochoice = "l"
+            
+        elif reply == "High":
+            resochoice = "h"
+            
+        #exit on cancel
+        else:
+            break
+        
+        #try to download
+        try:
+            
+            msg = "The download will begin.\n\nYou can watch the progress in your home directory.\nThis may take some time."
+            title = "Download Confirm"
+            
+            #user chose continue
+            if ccbox(msg, title):
+                #begin download
+                download_video(link, resochoice)
+                msgbox("Download has completed! the saved video is in the home directory.")
+                break
+            
+            #user chose Cancel
+            else:  
+                break
+            
+            #begin download
+            download_video(link, resochoice)
+            msgbox("Download has completed! the saved video is in the home directory.")
+            break
+        
+        #ask user if they want to restart   
+        except:
+            
+            msg = "Downloading the video has failed. Press continue to restart. Cancel to exit."
+            title = "Download failed"
+            
+            #user chose continue
+            if ccbox(msg, title):
+                continue
+            
+            #user chose Cancel
+            else:  
+                break
+        
+    #if channel is selected
+    elif chanloop == ("Channel"):
+        
+        #Getting channel link.
+        text = "What is the youtube channels URL?"
+        title = "Channel URL Selection"
+        chanlink = enterbox(text,title)
+        
+        #exit on cancel
+        if chanlink == None:
+            break
+            
         # Getting resolution choice.
-    choices = ["Low","High"]
-    title = "Youtube Download Quality"
-    msg = "Download Quality?"
-    reply = choicebox(msg,title, choices = choices)
-    
-    #low will equal l and high will equal h
-    if reply == "Low":
-        resochoice = "l"
+        choices = ["Low","High"]
+        title = "Youtube Download Quality"
+        msg = "What will be the download quality?"
+        reply = choicebox(msg,title, choices = choices)
         
-    elif reply == "High":
-        resochoice = "h"
+        #low will equal l and high will equal h
+        if reply == "Low":
+            resochoice = "l"
+            
+        elif reply == "High":
+            resochoice = "h"
+            
+        else:
+            break
         
+        try:
+            
+            msg = "The download will begin.\n\nYou can watch the progress in your home directory.\nThis may take some time."
+            title = "Download Confirm"
+            
+            #user chose continue
+            if ccbox(msg, title):
+
+                #begin download
+                download_video(chanlink,resochoice)
+                msgbox("Download has completed! the saved channel is in the home directory.")
+                break
+            #user chose Cancel
+            else:  
+                break
+
+        #ask user if they want to restart   
+        except:
+            
+            msg = "Downloading the channel has failed. Press continue to restart. Cancel to exit."
+            title = "Download failed"
+            
+            #user chose continue
+            if ccbox(msg, title):
+                continue
+            #user chose Cancel
+            else:
+                break
+            
+    #exit input will exit    
     else:
-        exit()
-        
-    #Calling function
-    download_video(link, resochoice)
-    
-#if channel is selected
-elif chanloop == ("Channel"):
-    
-    #Getting channel link.
-    text = "What is the Channels url?"
-    title = "channel url selection"
-    chanlink = enterbox(text,title)
-    
-    #exit on cancel
-    if chanlink == None:
-        exit()
-        
-    # Getting resolution choice.
-    choices = ["Low","High"]
-    title = "Youtube Download Quality"
-    msg = "Download Quality?"
-    reply = choicebox(msg,title, choices = choices)
-    
-    #low will equal l and high will equal h
-    if reply == "Low":
-        resochoice = "l"
-        
-    elif reply == "High":
-        resochoice = "h"
-        
-    else:
-        exit()
-        
-    # Calling function.
-    download_channel(chanlink,resochoice)
-    
-else:
-    exit()
+        break
